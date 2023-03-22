ab9b8191645edfe4bc78735d30235f962d2ba95e
dec-lied <gavindhondt@hotmail.com>
2023-03-21 21:26:23 -0400

    initial commit

--- main.ts
+++ main.ts
@@ -1,137 +1,175 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+import {
+	Decoration,
+	DecorationSet,
+	EditorView,
+	PluginSpec,
+	PluginValue,
+	ViewPlugin,
+	ViewUpdate,
+	WidgetType,
+} from "@codemirror/view";
 
-interface MyPluginSettings {
-	mySetting: string;
+import { syntaxTree } from "@codemirror/language";
+import { RangeSetBuilder } from "@codemirror/state";
+
+export class EmojiWidget extends WidgetType
+{
+	text: string;
+
+	constructor(text: string)
+	{
+		super();
+
+		this.text = text;
+	}
+
+	toDOM(view: EditorView): HTMLElement
+	{
+		const span = document.createElement("span");
+		span.addClass("test_class");
+		span.innerHTML = this.text;
+	
+		return span;
+	}
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
+export default class MyPlugin extends Plugin
+{
 	settings: MyPluginSettings;
 
-	async onload() {
+	async onload()
+	{
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		const pluginSpec: PluginSpec<EmojiListPlugin> = { decorations: (value: EmojiListPlugin) => value.decorations };
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		const emojiListPlugin = ViewPlugin.fromClass(EmojiListPlugin, pluginSpec);
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerEditorExtension(emojiListPlugin);
 	}
 
-	onunload() {
+	onunload()
+	{
 
 	}
 
-	async loadSettings() {
+	async loadSettings()
+	{
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
-	async saveSettings() {
+	async saveSettings()
+	{
 		await this.saveData(this.settings);
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+class EmojiListPlugin implements PluginValue
+{
+	decorations: DecorationSet;
+
+	constructor(view: EditorView)
+	{
+		this.decorations = this.buildDecorations(view);
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	update(update: ViewUpdate)
+	{
+		if (update.docChanged || update.viewportChanged)
+		{
+			this.decorations = this.buildDecorations(update.view);
+		}
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	destroy() {}
+
+	buildDecorations(view: EditorView): DecorationSet
+	{
+		const builder = new RangeSetBuilder<Decoration>();
+
+		console.log(view.visibleRanges);
+
+		for (let { from, to } of view.visibleRanges)
+		{
+			const view_string: string = view.state.sliceDoc(from, to);
+			console.log("string:", view_string);
+
+			const matchRegex = /236P/g; // regular expression to match all occurrences of "236P"
+
+			let match;
+			while ((match = matchRegex.exec(view_string)))
+			{
+				console.log("match", match);
+
+				const matchStart = from + view_string.indexOf("236P");
+				const matchEnd = matchStart + 4
+
+				builder.add(
+					from + matchStart,
+					from + matchEnd,
+					Decoration.replace(
+					{
+						widget: new EmojiWidget("replaced"),
+					})
+				);
+			}
+
+			// syntaxTree(view.state).iterate(
+			// {
+			// 	from,
+			// 	to,
+			// 	enter(node)
+			// 	{
+			// 		console.log("to", to, "from", from, "node", node.type, "line", line);
+			// 		console.log(view.state.doc.line(line + 1));
+			// 	},
+			// });
+		}
+
+		return builder.finish();
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+interface MyPluginSettings
+{
+	mySetting: string;
 }
+
+const DEFAULT_SETTINGS: MyPluginSettings =
+{
+	mySetting: 'default'
+}
+
+// export const stateField = StateField.define<DecorationSet>(
+// {
+// 	create(state): DecorationSet
+// 	{
+// 		return Decoration.none;
+// 	},
+
+// 	update(oldState, transaction): DecorationSet
+// 	{
+// 		const build = new RangeSetBuilder<Decoration>();
+		
+// 		return build.finish();	
+// 	},
+
+// 	provide(field: StateField<DecorationSet>): Extension
+// 	{
+// 		return EditorView.decorations.from(field);
+// 	}
+// })
+
+// export class FGCWidget extends WidgetType
+// {
+// 	toDOM(view: EditorView): HTMLElement
+// 	{
+// 		const div = document.createElement("span");
+	
+// 		div.innerText = "👉";
+	
+// 		return div;
+// 	}
+// }
